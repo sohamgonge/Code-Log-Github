bb03c593b450e2729b2f2f450f84a8bd762a40a1
Soham gonge <95542168+sohamgonge@users.noreply.github.com>
2025-07-03 23:11:41 +0530

    Move and update JavaScript loop example file
    Deleted the empty script.js from 45_ and added a new script.js in 45_Loops with various loop examples and console outputs.

---
+++ WebDev/JavaScript/45_Loops/script.js
@@ -0,0 +1,34 @@
+console.log("Iam a loop file");
+
+// let a = 1;
+// // console.log(a);
+// // console.log(a+1);
+// // console.log(a+2);
+
+// for (let i = 0; i < 100; i++) {
+//     console.log(a + i);
+// }
+
+// let obj = {
+//     name: "Soham",
+//     role: "Operating System Developer",
+//     company: "Apple"
+// }
+// for (const key in obj) {
+//     const element = obj[key];
+//     console.log(key);
+// }
+
+// for (const c of "Soham") {
+//     console.log(c);
+// }
+// let i = 0;
+// while (i<6) {
+//     console.log(i);
+//     i++;
+// }
+let i = 10;
+do {console.log(i);
+        i++;
+    
+} while (i<6);
