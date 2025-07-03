c3b2c99ac12c2e048158dce48ed90b420b597015
Soham gonge <95542168+sohamgonge@users.noreply.github.com>
2025-07-03 23:20:32 +0530

    Added example function and console logs for greetings
    Created a new JavaScript file with a 'nice' function and several console.log statements demonstrating personalized greetings.

---
+++ WebDev/JavaScript/46_Functions/index.js
@@ -0,0 +1,9 @@
+function nice(name) {
+    console.log("hey" + name + "you are nice!");
+    
+}
+
+console.log("Hey Soham you are nice!");
+console.log("Hey Soham you are good!");
+console.log("Hey Soham your tshirt is good!");
+console.log("Hey Soham your course is good too!");
