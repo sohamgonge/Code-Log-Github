404750b6ee463aaab84afda57f20b83ccfd1be91
Soham gonge <95542168+sohamgonge@users.noreply.github.com>
2025-07-05 01:32:26 +0530

    Add HTML and script for Faulty Calculator project
    Created index.html to load the calculator script. Updated script.js to prompt for user input and randomly perform either the correct or a faulty calculation by swapping operators with a 10% chance.

--- WebDev/JavaScript/47_Faulty_Calculator/script.js
+++ WebDev/JavaScript/47_Faulty_Calculator/script.js
@@ -8,3 +8,27 @@
 / -----> **
 */
 
+let random = Math.random();
+let a = prompt ("Enter first number");
+let b = prompt ("Enter Second number");
+let c = prompt ("Enter Third number");
+
+let obj ={
+    "+": "-",
+    "*": "+",
+    "-": "/",
+    "/": "**",
+}
+
+if (random>0.1){
+    // Perform correct operation
+    alert(`The result is ${eval(`${a} ${c} ${b}`)}`);
+    
+    
+}
+
+else {
+    // Perform faulty operation
+    c=obj[c];
+    alert(`The result is ${eval(`${a} ${c} ${b}`)}`);
+}
