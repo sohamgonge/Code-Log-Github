9dd29d570097ffea6e21b31a6d649fe2a845f97a
Soham gonge <95542168+sohamgonge@users.noreply.github.com>
2025-10-28 13:42:09 +0530

    moved files

--- JSPM/PYTHON_JSPM/Problems/tempCodeRunnerFile.py
+++ JSPM/PYTHON_JSPM/Problems/tempCodeRunnerFile.py
@@ -1,7 +1,1 @@
-salary=int(input("Enter your Salary in INR:"))
-if(salary<=10000):
-    print("Your total salary + bonus is: "+ str(salary+(salary*0.1)))
-elif(salary>10000 & salary<=20000):
-    print("Your total salary + bonus is: "+ str(salary+(salary*0.2)))
-elif(salary>20000):
-    print("Your total salary + bonus is: "+ str(salary+(salary*0.4)))
+print("")
