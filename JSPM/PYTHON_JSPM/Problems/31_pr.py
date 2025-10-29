9b7856ed3e0685b975c98c6af58c7147e237dc45
Soham gonge <95542168+sohamgonge@users.noreply.github.com>
2025-10-29 13:31:46 +0530

    Add string manipulation problem solutions 26-32
    Added new Python scripts (26_pr.py to 32_pr.py) for various string manipulation tasks, including counting characters, vowels/consonants, words, replacing spaces, changing case, and printing characters. These files provide solutions for problems 26 to 32 in the Problems directory.

---
+++ JSPM/PYTHON_JSPM/Problems/31_pr.py
@@ -0,0 +1,7 @@
+a = input("Enter : ")
+length = len(a)
+# print(a[1])
+spaces = 0
+for i in range (0, length):
+    print(a[i])
+print(f"all the spaces have been replaced by - in the given sentence and it looks like this : {a}")
