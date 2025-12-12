badb44cc3c7c819a9b0279e514ebe8c8dea5dff7
Soham gonge <95542168+sohamgonge@users.noreply.github.com>
2025-12-12 17:24:38 +0530

    Add debug prints and fix file rewind in quiz.c
    Added multiple printf statements for debugging to trace the flow inside loops. Fixed the usage of rewind to correctly reset the file pointer with rewind(qptr). Minor formatting improvements for readability.

--- JSPM/C_JSPM/Projects/quiz.c
+++ JSPM/C_JSPM/Projects/quiz.c
@@ -4,37 +4,43 @@ int main()
 {
     FILE *qptr;
     qptr = fopen("questions.txt", "r");
-    int counta = 0, ansa = 0, answered = 1, backslash = 1, totalback=0, start = 1,i = 0,n=0;
+    int counta = 0, ansa = 0, answered = 1, backslash = 1, totalback = 0, start = 1, i = 0, n = 0, r = 1;
     char c = fgetc(qptr);
-    int qlist[]={1,5,9,13,17};
+    int qlist[] = {1, 5, 9, 13, 17};
     while (answered <= 5)
     {
+        printf("%d\n",r);
+        printf("Inside Big While\n");
         for (backslash = 0; backslash < 3;)
         {
-            if (i==qlist[n])
+            printf("Inside Big For\n");
+            if (i == qlist[n])
             {
                 if (c == '\n')
                 {
+                    printf("Inside small IF1\n");
                     backslash++;
                 }
                 printf("%c", c);
                 answered++;
+                printf("Inside Big IF 1\n");
             }
             c = fgetc(qptr);
-            if (c=='\n')
+            if (c == '\n')
             {
                 totalback++;
-                if (totalback%3==0)
+                if (totalback % 3 == 0)
                 {
+                    printf("Inside small IF2\n");
                     i++;
                     n++;
                 }
-                
+
+                printf("Inside Big IF 2\n");
             }
-            
         }
-        rewind();
-        totalback=0;
+        rewind(qptr);
+        totalback = 0;
     }
     return 0;
 }
