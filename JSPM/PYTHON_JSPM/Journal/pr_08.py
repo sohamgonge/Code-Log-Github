e3dc81192735358da1583d3d948e7ecc56da636d
Soham gonge <95542168+sohamgonge@users.noreply.github.com>
2025-12-22 21:02:02 +0530

    Update pr_08.py

--- JSPM/PYTHON_JSPM/Journal/pr_08.py
+++ JSPM/PYTHON_JSPM/Journal/pr_08.py
@@ -2,14 +2,17 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import statistics as st
+import os
 
 # Step 1: Read the dataset
-data = pd.read_csv(r"Code-Log-Github/JSPM/PYTHON_JSPM/Journal/iris.csv")
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+CSV_PATH = os.path.join(BASE_DIR, "iris.csv")
+data = pd.read_csv(CSV_PATH)
 print(data.head())
 print(data.tail())
 print("Mean:",np.mean(data["sepal_length"]))
 print("Median:",np.median(data["sepal_length"]))
-print("Mode:",st.mode(data["sepal_length"]))
+print("Mode:", data["sepal_length"].mode().iloc[0])
 
 
 # Step 2: Select a numeric column for histogram
@@ -21,6 +24,7 @@ values = data["sepal_length"]
 
 # Step 3: Plot histogram
 plt.hist(values, bins=10, edgecolor="black")
+
 
 plt.title("Histogram of Sepal Length")
 plt.xlabel("Sepal Length (cm)")
