cc62bb6f7cc9a4e4a71b66360b26ca2fb2f54e24
Soham gonge <95542168+sohamgonge@users.noreply.github.com>
2026-01-05 23:17:30 +0530

    Refactor comparison logic to use inequality check
    Simplified the comparison between x and y by replacing multiple if statements with a single if-else that checks if x is not equal to y. This makes the code more concise and easier to read.

--- Python/CS50/conditional/compare.py
+++ Python/CS50/conditional/compare.py
@@ -1,9 +1,19 @@
 x = int(input("What's x? "))
 y = int(input("What's y? "))
 
-if x < y:# boolean equation x < y 
-    print("x is less than y")
-if x > y:
-    print("x is greater than y")
-if x == y:
+# if x < y:# boolean equation x < y 
+#     print("x is less than y")
+# elif x > y:
+#     print("x is greater than y")
+# elif x == y:
+#     print("x is equal to y")
+
+# if x>y or x<y:
+#     print("x is not equal to y")
+# else:
+#     print("x is equal to y")
+
+if x!=y:
+    print("x is not equal to y")
+else:
     print("x is equal to y")
