ae1ae712630831fa51bb5bb01c89da3eccb6fec2
Soham gonge <95542168+sohamgonge@users.noreply.github.com>
2026-01-14 23:57:41 +0530

    Add VS Code C++ config and update main.cpp loop
    Added VS Code configuration files for C++ development, including build and launch settings. Updated main.cpp to introduce a loop counter variable. Also added and updated debug symbol files for new and existing binaries.

--- CodeVita/main.cpp
+++ CodeVita/main.cpp
@@ -7,9 +7,10 @@ using namespace std;
 int main()
 {
     vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
-    
+    int i = 0;
     for (const string& word : msg)
     {
+        ++i;
         cout << word << " ";
     }
     cout << endl;
