67c17a418e4a3bfac1daa4b10fb14d6311d1f5e7
Soham gonge <95542168+sohamgonge@users.noreply.github.com>
2026-01-14 21:51:43 +0530

    Refactor C++ main and update VS Code tasks
    Enhanced main.cpp to print a vector of strings instead of a static message. Updated .vscode/tasks.json to use a bash command for building and running C files, and removed unused launch.json. Added debug symbol files and binary artifacts for CodeVita/main.

--- CodeVita/main.cpp
+++ CodeVita/main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
+#include <vector>
+#include <string>
+
 using namespace std;
 
 int main()
 {
-    cout << "Hello, world!!!";
-
-    return 0;
+    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+    
+    for (const string& word : msg)
+    {
+        cout << word << " ";
+    }
+    cout << endl;
 }
