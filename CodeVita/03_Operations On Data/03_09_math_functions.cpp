5527d1f2e1dcab5c5c0bfc37de0cace421f600f3
Soham gonge <95542168+sohamgonge@users.noreply.github.com>
2026-01-21 16:09:23 +0530

    Add examples for output formatting, numeric limits, and math functions
    Expanded 03_07_output_formating.cpp with detailed examples of C++ output manipulators. Added 03_08_numeric_limits.cpp to demonstrate usage of std::numeric_limits for various types. Added 03_09_math_functions.cpp with a basic example of using std::floor from <cmath>.

---
+++ CodeVita/03_Operations On Data/03_09_math_functions.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+    double weight { 7.7 };
+    
+    // floor
+    std::cout << "Weight rounted to floor is : " << std::floor(weight) << std::endl;
+    
+    return 0;
+}
