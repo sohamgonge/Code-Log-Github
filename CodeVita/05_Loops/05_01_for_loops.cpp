b49a16132ebc6f6f2734fe92468fc4e2036831ae
Soham gonge <95542168+sohamgonge@users.noreply.github.com>
2026-01-23 07:03:22 +0530

    Add examples for for, while, and do-while loops
    Expanded 05_01_for_loops.cpp with detailed for loop usage and best practices. Added new files demonstrating while and do-while loop constructs with sample code and comments for educational purposes.

--- CodeVita/05_Loops/05_01_for_loops.cpp
+++ CodeVita/05_Loops/05_01_for_loops.cpp
@@ -2,6 +2,80 @@
 
 int main()
 {
-    size_t i{}; 
+    /*
+    std::cout << "I love C++" << std::endl;
+    std::cout << "I love C++" << std::endl;
+    std::cout << "I love C++" << std::endl;
+    std::cout << "I love C++" << std::endl;
+    std::cout << "I love C++" << std::endl;
+    std::cout << "I love C++" << std::endl;
+    std::cout << "I love C++" << std::endl;
+    std::cout << "I love C++" << std::endl;
+    std::cout << "I love C++" << std::endl;
+    std::cout << "I love C++" << std::endl;
+    */
+
+    // for loop : the good way
+
+    /*
+    for( unsigned int i {0} ; i < 10000 ; ++i){
+        // Whatever we want the loop to run
+        std::cout << i << " : I love C++" << std::endl;
+    }
+    std::cout << "Loop done!" << std::endl;
+    */
+
+    // Use size_t : a representation of some unsigned int for positive numbers [sizes]
+
+    /*
+    for( size_t i {0} ; i < 10 ; ++i){// even used in c++ standard library
+        std::cout << i << " : I love C++" << std::endl;
+    }
+    std::cout << "loop done" << std::endl;
+    */
+
+    /*
+    // stzeof(size_t)
+    std::cout << "sizeof(size_t) : " << sizeof(size_t) << std::endl;
+    */
+    
+    /*
+    // Scope of iterator
+    for(size_t i{0}; i < 10 ; ++i){
+        std::cout << i << " : I love C++" << std::endl;
+    }
+    std::cout << "Loop done!" << std::endl;
+    // std::cout << "i : " << i << std::endl; Compiler error : i is not in scope
+    */
+
+    /*
+    // Iterator declared outside the loop
+    size_t i{0}; // Iterator defined outside
+    for(i ; i < 10 ; ++i){
+        std::cout << i << " : I love C++" << std::endl;
+    }
+    std::cout << "Loop done!" << std::endl;
+    std::cout << "i : " << i << std::endl;
+    */
+
+    /*
+    // Leave out the iterator declaration part
+    size_t i{0}; // Iterator defined outside
+    for( ; i < 10 ; ++i){
+        std::cout << i << " : I love C++" << std::endl;
+    }
+    std::cout << "Loop done!" << std::endl;
+    std::cout << "i : " << i << std::endl;
+    */
+
+    // Don't hard code values : BAD!
+
+    const size_t COUNT{10};
+
+    for(size_t i{0}; i < COUNT ; ++i){
+        std::cout << i << " : I love C++" << std::endl;
+    }
+    std::cout << "Loop done!" << std::endl;
+    
     return 0;
 }
