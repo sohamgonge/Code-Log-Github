9f1eecbd041c15b302624d823a406ca6e35898e0
Soham gonge <95542168+sohamgonge@users.noreply.github.com>
2026-03-05 00:14:11 +0530

    minor changes

--- DSA/10_Painter_Problem/10_painterProblem.cpp
+++ DSA/10_Painter_Problem/10_painterProblem.cpp
@@ -1,34 +1,28 @@
 #include <iostream>
 #include <vector>
+
 using namespace std;
 
-bool isValid(vector<int> &arr, int n, int m, int maxAllowedPages)
+bool isValid(vector<int> &arr, int n, int m, int maxTimeAllowed)
 {
-    int students = 1, pages = 0;
-
+    int painterno = 1, time = 0;
     for (int i = 0; i < n; i++)
     {
-        if (arr[i] > maxAllowedPages)
+        if (arr[i] > maxTimeAllowed)
         {
             return false;
         }
-
-        if (pages + arr[i] <= maxAllowedPages)
+        if (time + arr[i] <= maxTimeAllowed)
         {
-            pages += arr[i];
+            time += arr[i];
         }
         else
         {
-            students++;
-            pages = arr[i];
+            painterno++;
+            time = arr[i];
         }
     }
-    if (students > m)
-    {
-        return false;
-    }
-
-    if (students > m)
+    if (painterno > m)
     {
         return false;
     }
@@ -38,29 +32,26 @@ bool isValid(vector<int> &arr, int n, int m, int maxAllowedPages)
     }
 }
 
-int allocateBooks(vector<int> &arr, int n, int m)
+int painterPartition(vector<int> &arr, int n, int m)
 {
     if (m > n)
     {
         return -1;
     }
     int sum = 0;
-    int ans = -1;
+    int ans = 0;
     for (int i = 0; i < n; i++)
     {
         sum += arr[i];
     }
-
-    int st = 0, end = sum; // range of possible ans;
-
+    int st = 0, end = sum;
     while (st <= end)
     {
         int mid = st + (end - st) / 2;
-
         if (isValid(arr, n, m, mid))
         {
-            ans = mid;
             end = mid - 1;
+            ans = mid;
         }
         else
         {
@@ -74,7 +65,6 @@ int main()
 {
     vector<int> arr = {40, 30, 10, 20};
     int n = 4, m = 2;
-
-    cout << allocateBooks(arr, n, m) << endl;
+    cout << painterPartition(arr, n, m) << endl;
     return 0;
 }
